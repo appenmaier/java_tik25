40ce30b8704b4747ae2d92c6f037513c6e3e636a
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-04-21 15:11:02 +0200

    Add Cases, Branches and Loops to Demo 1

--- src/main/java/main/D01_JavaBasics.java
+++ src/main/java/main/D01_JavaBasics.java
@@ -2,9 +2,22 @@ package main;
 
 import java.util.Scanner;
 
+/**
+ * Java Basics
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
 public class D01_JavaBasics {
 
+   /**
+    * main method of Java Basics class
+    * 
+    * @param args input arguments
+    */
    public static void main(String[] args) {
+      /* Datenobjekte und Datentypen */
       String firstName;
       String lastName;
       String firstNameAndLastName;
@@ -49,6 +62,54 @@ public class D01_JavaBasics {
       System.out.println(gender);
       System.out.println(firstNameAndLastName);
       System.out.printf("%S %S%n", firstName, lastName);
+
+      // Vergleichsoperatoren: <, <=, >, >=, ==, !=
+      // Logische Operatoren: UND (&&), ODER (||), VERNEINUNG (!)
+      if ((gender == 'm' || gender == 'M') && age >= 18) {
+         System.out.println("Hallo Herr " + lastName);
+      } else if ((gender == 'w' || gender == 'W') && age >= 18) {
+         System.out.println("Hallo Frau " + lastName);
+      } else {
+         System.out.println("Hallo " + firstName);
+      }
+
+      String happyText;
+      if (isHappy) {
+         happyText = "glücklich";
+      } else {
+         happyText = "nicht glücklich";
+      }
+      System.out.println("Du bist " + happyText);
+
+      happyText = (isHappy) ? "glücklich" : "nicht glücklich";
+      System.out.println("Du bist " + happyText);
+
+      System.out.println("Du bist " + ((isHappy) ? "glücklich" : "nicht glücklich"));
+
+      /* Fallunterscheidungen */
+      System.out.println(switch (gender) {
+         case 'm', 'M' -> "männlich";
+         case 'w', 'W' -> "weiblich";
+         case 'd', 'D' -> "divers";
+         default       -> gender;
+      });
+
+      /* Schleifen */
+      int x = 0;
+      while (x < 10) {
+         System.out.println(x + 1);
+         x++;
+      }
+
+      int z = 0;
+      do {
+         System.out.println(z + 1);
+         z++;
+      } while (z < 10);
+
+      for (int i = 0; i < 10; i++) {
+         System.out.println(i + 1);
+      }
    }
 
 }
